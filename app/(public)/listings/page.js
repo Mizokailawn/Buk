76a4525b429c7b488c50b7b9ae6e1ee14d5f126b
MizoKailawn <mizokailawn@gmail.com>
2026-06-22 00:32:44 +0530

    refactor metadata

--- app/(public)/listings/page.js
+++ app/(public)/listings/page.js
@@ -10,6 +10,72 @@ import { GetVehicleFilterOptions } from "@/lib/queries/vehicles/get-vehicle";
 import { SearchProvider } from "@/components/search/search-provider";
 import { Suspense } from "react";
 
+export async function generateMetadata({ searchParams }) {
+  const params = await searchParams;
+  
+  const brand = params.brand;
+  const model = params.model;
+  const category = params.category;
+  const city = params.city;
+  
+  function capitalize(str) {
+    if (!str) return "";
+
+    return str
+    .split("")
+    .map(
+      (w) =>
+        w.charAt(0).toUppercase() +
+        w.slice(1).toLowercase()
+    )
+    .join("")
+  }
+  
+  const brandName = capitalize(brand)
+  const modelName = capitalize(model)
+
+
+  let title = "Used Vehicles in for Sale in Mizoram · BUK";
+  let description =
+    "Buy and sell used cars, bikes, scooters and other vehicles across Mizoram.";
+
+  if (brand && model) {
+    title = `${brandName} ${modelName} Listings · BUK`;
+    description = `Browse available ${brandName} ${modelName} listings on BUK.`;
+  } else if (brand) {
+    title = `${brandName} Vehicles for Sale · BUK`;
+    description = `Browse used ${brandName} vehicles for sale across Mizoram on BUK.`;
+  } else if (category && city) {
+    title = `${category}s for Sale in ${city} · BUK`;
+    description = `Browse used ${category} listings in ${city} on BUK.`;
+  } else if (category) {
+    title = `${category}s for Sale · BUK`;
+    description = `Browse used ${category} listings across Mizoram on BUK.`;
+  } else if (city) {
+    title = `Vehicles for Sale in ${city} · BUK`;
+    description = `Browse used vehicle listings in ${city} on BUK.`;
+  }
+
+  return {
+    title,
+    description,
+
+    openGraph: {
+      title,
+      description,      
+      type: "website",      
+      images: [
+        {
+          url: "/icon-512.png",
+          width: 512,
+          height: 512,
+          alt: "BUK Logo",
+        },
+      ],
+    },    
+  };
+}
+
 export default async function ListingsPage({ searchParams }) {
   const filterOptions = await GetVehicleFilterOptions();
 
